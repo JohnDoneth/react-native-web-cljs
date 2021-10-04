8e3745674f691d2109e7711726a22e02ab3c294a
JohnDoneth <doneth7@gmail.com>
2021-10-04 19:17:38 -0400

    Code cleanup

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,3 @@
 module.exports = {
-  stories: ['../public/js/stories/**/*stories.js']
+  stories: ["../public/js/stories/stories.*.js"],
 };
--- src/components/header.cljs
+++ src/components/header.cljs
@@ -1,20 +1,29 @@
 (ns components.header
-  (:require ["react-native" :as rn]))
+  (:require ["react-native" :as rn]
+            [clojure.walk]))
+
+(def stylesheet (.-StyleSheet rn))
+
+(defn create-stylesheet [styles]
+  (->> styles
+       (clj->js)
+       (.create stylesheet)
+       (js->clj)
+       (clojure.walk/keywordize-keys)))
 
 (def styles
-  ^js (-> {:container
-           {:flex 1
-            :backgroundColor "#fff"
-            :alignItems "center"
-            :justifyContent "center"}
-           :title
-           {:fontWeight "bold"
-            :fontSize 24
-            :color "blue"}}
-          (clj->js)
-          (rn/StyleSheet.create)))
+  (create-stylesheet
+   {:container
+    {:flex 1
+     :backgroundColor "#fff"
+     :alignItems "center"
+     :justifyContent "center"}
+    :title
+    {:fontWeight "bold"
+     :fontSize 24
+     :color "blue"}}))
 
 (defn header [text]
-  [:> rn/View {:style (.-container styles)}
-   [:> rn/Text {:style (.-title styles)} text]])
+  [:> rn/View {:style (:container styles)}
+   [:> rn/Text {:style (:title styles)} text]])
 
